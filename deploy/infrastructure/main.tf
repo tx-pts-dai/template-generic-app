988bdaf5eca0bfe24ccd3d82d7d8cfe9df0b4278
Demetrio Carrara <carrarademetrio@gmail.com>
2024-08-29 16:15:04 +0200

    Update main.tf

--- deploy/infrastructure/main.tf
+++ deploy/infrastructure/main.tf
@@ -56,13 +56,12 @@ module "acm" {
 
   domain_name = local.app_url
 
-  {% if dns_provider == "aws" %}
+{% if dns_provider == "aws" %}
   zone_id = data.aws_route53_zone.this.zone_id
-
-  {% elif dns_provider == "cloudflare" %}
+{% elif dns_provider == "cloudflare" %}
   create_route53_records  = false
   validation_record_fqdns = cloudflare_record.validation[*].hostname
-  {%- endif %}
+{% endif %}
 
   validation_method = "DNS"
 }
